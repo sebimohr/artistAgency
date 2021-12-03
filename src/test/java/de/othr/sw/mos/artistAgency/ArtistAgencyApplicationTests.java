707c2813c2af1c2d62b23d1210c8c6f65f9f5e26
Sebastian Mohr <sebastianmohr96@gmail.com>
2021-12-03 12:04:06 +0100

    init project with spring framework

---
+++ src/test/java/de/othr/sw/mos/artistAgency/ArtistAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.othr.sw.mos.artistAgency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
